266662ae5d62f173f3007b49ed677022801dd50e
Mykhailo Tanasov <mtanasov@gmail.com>
2023-05-04 20:27:10 +0300

    edit

--- app/head.tsx
+++ app/head.tsx
@@ -2,11 +2,16 @@ export default function Head() {
   return (
     <>
       <title> iOS 15 Clock </title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
+        type="image/png"
+        sizes="16x16"
         rel="icon"
-        href="http://s1.iconbird.com/ico/0612/VistaStyleSportIconSet/w256h2561339399114Stopwatch.png"
+        // href="https://s1.iconbird.com/ico/0612/VistaStyleSportIconSet/w256h2561339399114Stopwatch.png"
+        // href="../../img/favicon.png"
+        href="https://cdn-icons-png.flaticon.com/512/269/269054.png"
       />
+      {/* <link type="image/png" sizes="16x16" rel="icon" href="/favicon-16x16.png"></link> */}
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   );
 }
